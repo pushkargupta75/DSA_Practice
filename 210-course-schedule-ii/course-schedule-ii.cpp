98d9641579f186d431db65953d9df5dff04276a2
PUSHKAR GUPTA <pushkargupta993@gmail.com>
2025-11-30 15:52:02 +0530

    Time: 7 ms (34.31%) | Memory: 18.3 MB (72.54%) - LeetSync

---
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<int> canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> adj(numCourses);
+        
+        vector<int> indegree(numCourses,0);
+        for (auto &pr:prerequisites) {
+            int course=pr[0];
+            int pre=pr[1];
+            adj[pre].push_back(course);
+            indegree[course]++;
+        }
+        queue<int> q;
+        for(int i=0;i<numCourses;i++){
+            if(indegree[i]==0) q.push(i);
+        }
+        vector<int> topo;
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            topo.push_back(node);
+            for(auto it: adj[node]){
+                indegree[it]--;
+                if(indegree[it]==0) q.push(it);
+            }
+        }
+        if(topo.size()!=numCourses) return {};
+
+        return topo;
+    }
+
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        return canFinish(numCourses,prerequisites);
+    }
+};
