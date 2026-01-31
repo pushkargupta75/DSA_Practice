4ac5c3fab8300568892f39fd00d8ca5f6dcd0133
PUSHKAR GUPTA <pushkargupta993@gmail.com>
2026-01-31 13:13:02 +0530

    Time: 0 ms (100.00%) | Memory: 19.8 MB (62.61%) - LeetSync

--- 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.cpp
+++ 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.cpp
@@ -5,7 +5,6 @@ public:
         int end=letters.size()-1;
         while(beg<=end){
             int mid=beg+(end-beg)/2;
-           
             if(letters[mid]<=target) beg=mid+1;
             else end=mid-1;
         }
