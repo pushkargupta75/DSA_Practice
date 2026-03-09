639b311a24cf66487661b8ed283d2419e9a82935
PUSHKAR GUPTA <pushkargupta993@gmail.com>
2026-03-09 21:55:10 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (82.53%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int reverse(int x) {
+        long long int rev=0;
+        while(x!=0){
+            rev=rev*10+x%10;
+            x/=10;
+        }
+        return (rev<INT_MIN || rev> INT_MAX)?0:rev;
+
+    }
+};
