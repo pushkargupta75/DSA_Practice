6a7d43bde615e54534858f278a0e1097d982d54e
PUSHKAR GUPTA <pushkargupta993@gmail.com>
2026-04-23 14:16:16 +0530

    Time: 0 ms (100.00%) | Memory: 20.2 MB (98.30%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,20 +1,31 @@
 class Solution {
 public:
-    int helper(int i,vector<int>& nums,vector<int> &dp,int n){
-        if(i>=n-1) return 0;
-        if(dp[i]!=-1) return dp[i];
-        int far=min(i+nums[i],n-1);
-        int ans=INT_MAX;
-        for(int idx=i+1;idx<=far;idx++){
-            int res=helper(idx,nums,dp,n);
-            if(res!=INT_MAX) ans=min(ans,1+res);
-        }
-        return dp[i]=ans;
+    // int helper(int i,vector<int>& nums,vector<int> &dp,int n){
+    //     if(i>=n-1) return 0;
+    //     if(dp[i]!=-1) return dp[i];
+    //     int far=min(i+nums[i],n-1);
+    //     int ans=INT_MAX;
+    //     for(int idx=i+1;idx<=far;idx++){
+    //         int res=helper(idx,nums,dp,n);
+    //         if(res!=INT_MAX) ans=min(ans,1+res);
+    //     }
+    //     return dp[i]=ans;
         
-    }
+    // }
     int jump(vector<int>& nums) {
         int n=nums.size();
-        vector<int> dp(n,-1);
-        return helper(0,nums,dp,n);
+        // vector<int> dp(n,-1);
+        // return helper(0,nums,dp,n);
+        int jumps=0;
+        int maxreach=0;
+        int end=0;
+        for(int i=0;i<n-1;i++){
+            maxreach=max(maxreach,i+nums[i]);
+            if(i==end){
+                jumps++;
+                end=maxreach;
+            }
+        }
+        return jumps;
     }
 };
